efee853397f8a0ad09bd6ecb31a90b9c1997822c
romanchmelyck <roman.chmelyck@gmail.con>
2015-05-09 15:15:21 +0300

    Update ConsoleInterface.java

--- VinylPlatePhotoDecoder/src/com/imageDecoder/ConsoleInterface.java
+++ VinylPlatePhotoDecoder/src/com/imageDecoder/ConsoleInterface.java
@@ -11,37 +11,37 @@ public class ConsoleInterface {
 		Scanner scanIn = new Scanner(System.in);
 		String input;
 			
-				while(true){	
+			while(true){	
 					
-							System.out.println("Please select the image to process:");
-							System.out.println("Enter " + 1 + ", " + 2 + " or " + 3 + " to make a choice or enter " + 0 + " to exit..."); 
+					System.out.println("Please select the image to process:");
+					System.out.println("Enter " + 1 + ", " + 2 + " or " + 3 + " to make a choice or enter " + 0 + " to exit..."); 
 						
-							input = scanIn.nextLine();
+					input = scanIn.nextLine();
 						
-								switch (input){
+					switch (input){
 								
-										case "1": 	imageName = "images\\01.png";
-													break;
+							case "1": 	imageName = "images\\01.png";
+									break;
 										
-										case "2": 	imageName = "images\\02.png";
-													break;
+							case "2": 	imageName = "images\\02.png";
+									break;
 										
-										case "3": 	imageName = "images\\03.png";
-													break;
+							case "3": 	imageName = "images\\03.png";
+									break;
 										
-										case "0": 	scanIn.close();
-													System.out.println("Closing program.."); 
-													System.exit(0);
+							case "0": 	scanIn.close();
+									System.out.println("Closing program.."); 
+									System.exit(0);
 										
-										default: 	imageName = null;
-													System.out.println("Invalid input! Please enter valid value! Only 1, 2 and 3 allowed!");
-													break;					
-									}
+							default: 	imageName = null;
+									System.out.println("Invalid input! Please enter valid value! Only 1, 2 and 3 allowed!");
+									break;					
+							}
 						
-										if(imageName!= null){
-											decoder.makeAudioFile(imageName);
-										}
-						}
+						if(imageName!= null){
+							decoder.makeAudioFile(imageName);
+							}
+				}
 		
 		}
 
